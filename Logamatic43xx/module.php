bbedbef5bb86f6abe5938c74ced75a5b0d73eb00
Hermann <doorpi1@gmail.com>
2016-10-02 15:47:49 +0200

    Default host und port geändert

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -123,7 +123,7 @@ class Logamatic43xx extends IPSModule
         $data = json_decode($JSONString);
         IPS_LogMessage('Logamatic 43xx -> Gateway', utf8_decode($data->Buffer));
         $stream = utf8_decode($data->Buffer);
-        $data = utf8_encode(substr($stream, 0, 2).$this->ReadPropertyInteger('Bus').substr($stream, 2)); // ECO-CAN Busadresse einfügen
+        $data = utf8_encode(substr($stream, 0, 2).chr($this->ReadPropertyInteger('Bus')).substr($stream, 2)); // ECO-CAN Busadresse einfügen
         $id = $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
         return $id;
     }
