08cb2a9858e4664bd0cd45517513c60ac942d3d2
Hermann <doorpi1@gmail.com>
2016-10-02 14:46:39 +0200

    Default host und port geändert

--- FM442/module.php
+++ FM442/module.php
@@ -24,7 +24,7 @@ class FM442 extends IPSModule
     {
         $data = json_decode($JSONString);
         IPS_LogMessage('FM442 -> Logamatic', bin2hex(utf8_decode($data)));
-        $id = IPS_SendDataToParent( $this->InstanceID, json_encode(Array('DataID' => '{482A20C1-35A8-4591-96F0-C119AB72EBB2}', "Buffer" => utf8_encode($data)))); // Daten senden
+        $id = IPS_SendDataToParent( $this->InstanceID, json_encode(Array('DataID' => '{482A20C1-35A8-4591-96F0-C119AB72EBB2}', "Buffer" => $data))); // Daten senden
         return $id;
     }
 
