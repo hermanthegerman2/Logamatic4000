43b1e7ab45d2f45a0caf129893ad16fa1a6db250
Hermann <doorpi1@gmail.com>
2016-10-02 23:06:01 +0200

    Default host und port geändert

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -56,7 +56,7 @@ class Logamatic43xx extends IPSModule
 
     public function RequestMonitordaten()
     {
-        $data = utf8_encode(chr(Command::Direktmodus));
+        $data = utf8_encode(chr(Command::Direktmodus).chr(Command::NUL));
         $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
         $data = utf8_encode(chr(Command::Monitordaten).chr($this->ReadPropertyInteger('Bus')).chr(Command::NUL).chr(Command::NUL).chr(Command::NUL));
         $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
@@ -66,7 +66,7 @@ class Logamatic43xx extends IPSModule
 
     public function RequestEinstellPar()
     {
-        $data = utf8_encode(chr(Command::Direktmodus));
+        $data = utf8_encode(chr(Command::Direktmodus).chr(Command::NUL));
         $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
         $data = utf8_encode(chr(Command::Einstellparameter).chr($this->ReadPropertyInteger('Bus')).chr(Command::NUL).chr(Command::NUL).chr(Command::NUL));
         $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
