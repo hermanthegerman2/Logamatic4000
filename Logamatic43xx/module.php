f00f4e99baa724802497535e881e9303a0bc3b61
Hermann <doorpi1@gmail.com>
2016-10-03 12:13:53 +0200

    Default host und port geändert

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -138,14 +138,19 @@ class Logamatic43xx extends IPSModule
         IPS_LogMessage('Logamatic 43xx -> Gateway', bin2hex(utf8_decode($data->Buffer)));
         $stream = utf8_decode($data->Buffer);
         $datentyp = substr(bin2hex($stream), 0, 2);
-        if ($datentyp === 'b0') {
-            $this->SwitchDM();
-            $data = utf8_encode(substr($stream, 0, 1) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2) .chr(0x00)); // ECO-CAN Busadresse einfügen
-            $id = $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
-            sleep(2);
-            //$this->SwitchNM();
+        switch ($datentyp) {
+            case 'b0':
+                $this->SwitchDM();
+                $data = utf8_encode(substr($stream, 0, 1) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2) . chr(0x00)); // ECO-CAN Busadresse einfügen
+                $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
+                sleep(2);
+                $this->SwitchNM();
+                break;
+            case 'a7':
+                $this->ReceiveData($JSONString);
+                break;
         }
-        return $id;
+        return true;
     }
     
     public function ReceiveData($JSONString)
