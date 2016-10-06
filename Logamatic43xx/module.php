c228de24c22af6af0e807a4741cb58ba365498b3
Hermann <doorpi1@gmail.com>
2016-10-06 21:09:29 +0200

    Default host und port geändert

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -149,13 +149,13 @@ class Logamatic43xx extends IPSModule
         switch ($datentyp) {
             case 'b0':
                 $this->SwitchDM();
-                $offset = ord(substr(bin2hex($stream), 3, 1));
+                $offset = ord(substr($stream, 3, 1));
                 if ($offset = '0') $offset = $offset+1;
                 $data = utf8_encode(substr($stream, 0, 1) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2) . chr(0x00)); // ECO-CAN Busadresse einfügen
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 //$this->SwitchNM();
                 sleep (0.5);
-                $data = utf8_encode(chr(Command::Datenblock).chr($this->ReadPropertyInteger('Bus')).chr($datentyp).chr(0x00)); // Rückantwort anfragen
+                $data = utf8_encode(chr(Command::Datenblock).chr($this->ReadPropertyInteger('Bus')).$datentyp.$offset.chr(0x00)); // Rückantwort anfragen
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 break;
             case 'a7':
