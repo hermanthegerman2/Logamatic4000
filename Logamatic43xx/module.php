9c63e04c25866e81ee6656bd70a8765017842bd0
hermanthegerman2 <doetsch.hermann@t-online.de>
2017-02-05 23:11:08 +0100

    Umstellung auf Gateway3964R von Hicomer

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -157,7 +157,7 @@ class Logamatic43xx extends IPSModule
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 sleep (1);
                 $offset = substr($stream, 3, 1);
-                if ($offset = '00') $offset = '1';
+                //if ($offset = '0') $offset = '1';
                 $data = utf8_encode(chr(Command::Datenblock).chr($this->ReadPropertyInteger('Bus')).substr($stream, 2, 1).chr($offset)); // Rückantwort anfragen
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 sleep (0.5);
