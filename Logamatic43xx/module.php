fde8bbbceed1f462aebf5d54a458e90d6c6fed59
hermanthegerman2 <doetsch.hermann@t-online.de>
2017-02-06 22:12:33 +0100

    Umstellung auf Gateway3964R von Hicomer

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -157,9 +157,14 @@ class Logamatic43xx extends IPSModule
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 sleep (1);
                 $offset = substr(bin2hex($stream), 6, 2);
-                IPS_LogMessage('Buderus Logamatic', ord($offset));
-                if (ord($offset) == 48) $data = utf8_encode(chr(Command::Datenblock) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2, 1) . chr(Command::ONE)); // Rückantwort anfragen
-                    else $data = utf8_encode(chr(Command::Datenblock) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2, 1) . chr($offset)); // Rückantwort anfragen
+                IPS_LogMessage('Buderus Logamatic', $offset);
+                switch ($offset) {
+                    case '00':
+                        $data = utf8_encode(chr(Command::Datenblock) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2, 1) . chr(Command::ONE)); // Rückantwort anfragen
+                        break;
+                    default:
+                        $data = utf8_encode(chr(Command::Datenblock) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2, 1) . chr($offset)); // Rückantwort anfragen
+                }
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 sleep (0.5);
                 $this->SwitchNM();
