32faf6658a6cf58fc51ddcb4861b257fd725dbd4
hermanthegerman2 <doetsch.hermann@t-online.de>
2017-02-06 21:46:47 +0100

    Umstellung auf Gateway3964R von Hicomer

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -156,7 +156,7 @@ class Logamatic43xx extends IPSModule
                 $data = utf8_encode(substr($stream, 0, 1) . chr($this->ReadPropertyInteger('Bus')) . substr($stream, 2)); // ECO-CAN Busadresse einfügen
                 $this->SendDataToParent(json_encode(Array("DataID" => "{0D923A14-D3B4-4F44-A4AB-D2B534693C35}", "Buffer" => $data)));
                 sleep (1);
-                $offset = substr(bin2hex($stream), 3, 1);
+                $offset = substr(bin2hex($stream), 3, 2);
                 IPS_LogMessage('Buderus Logamatic', $offset);
                 switch ($offset) {
                     case '00':
