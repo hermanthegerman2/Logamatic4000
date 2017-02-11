e71757a0b106d434c98aebfa00c26962d9b86e2c
hermanthegerman2 <doetsch.hermann@t-online.de>
2017-02-11 23:15:53 +0100

    Umstellung auf Gateway3964R von Hicomer

--- Logamatic43xx/module.php
+++ Logamatic43xx/module.php
@@ -32,10 +32,10 @@ class Logamatic43xx extends IPSModule
                 $this->SetStatus(203);
                 break;
             case $id < 16:*/
-                //$this->MaintainVariable('Einstellparameter', 'Einstellparameter', 3, '~String', -3, 1);
-                //IPS_SetHidden($this->GetIDForIdent('Einstellparameter'), true);
-                //$this->MaintainVariable('Monitordaten', 'Monitordaten', 3, '~String', 0, 1);
-                //IPS_SetHidden($this->GetIDForIdent('Monitordaten'), true);
+                $this->MaintainVariable('Einstellparameter', 'Einstellparameter', 3, '~String', 0, 1);
+                IPS_SetHidden($this->GetIDForIdent('Einstellparameter'), true);
+                $this->MaintainVariable('Monitordaten', 'Monitordaten', 3, '~String', 0, 1);
+                IPS_SetHidden($this->GetIDForIdent('Monitordaten'), true);
                 $this->RegisterProfile('Minutes', '2', '', '', ' m', 0, 0, 0);
                 $this->RegisterProfile('Hours', '2', '', '', ' h', 0, 0, 0);
                 $this->RegisterProfile('Watt', '2', '', '', ' kWh', 0, 0, 0);
